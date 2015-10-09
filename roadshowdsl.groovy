c90ee9e83292c316c58769f9ea456da34886fcea
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2015-10-09 11:25:16 +0200

    Added functions, some of the best practices and comments
    Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

--- roadshowdsl.groovy
+++ roadshowdsl.groovy
@@ -1,47 +1,102 @@
-job("${GITHUB_USER}.roadshow.generated.build") {
-    scm {
-      git{
-        remote {
-          name('origin')
-          url("git@github.com:${GITHUB_USER}/roadshow.git")
+/*
+ * Use this function to enable must have-defaults for your jobs
+ * LogRotator - make sure that you are cleaning up old logs
+ * Timeout - kill job in case if it takes to long
+ *           This will indicate a coming problem for you and unclock
+ *           pipeline in case of hanging jobs
+ * Timestamps - just nice to have have feature. Adds time stamps
+ *              to console output
+ *
+ */
+def addDefaultParameters(def context, buildsToKeep=50, artifactsToKeep=10, timeoutVal=30) {
+    // Add timestamps and timeouts
+    context.wrappers {
+        timestamps()
+        timeout {
+            absolute(timeoutVal)
         }
-        branch("master")
-      }
     }
-    triggers {
-        scm('* * * * *')
-    }
-    steps {
-        gradle('clean war jenkinstest jacoco')
-      	shell("echo 'Hello, world!!'")
-    }
-  	publishers {
-      	jacocoCodeCoverage()
-      	archiveJunit('build/test-results/*.xml')
-      	warnings(['Java Compiler (javac)'])
-    	downstream("${GITHUB_USER}.roadshow.generated.staticanalysis", 'SUCCESS')
+    // Set log rotator
+    context.logRotator {
+        numToKeep(buildsToKeep)
+        artifactNumToKeep(artifactsToKeep)
     }
 }
 
-job("${GITHUB_USER}.roadshow.generated.staticanalysis") {
-    scm {
+/*
+ * Use this function to setup git clone to your repo
+ * repoURL - URL for repository to clone
+ * branch - branch name to checkout
+ *
+ */
+def addGitSCM(def context, repoURL, branchName='master') {
+    context.scm {
         git{
-          remote {
-            name('origin')
-            url("git@github.com:${GITHUB_USER}/roadshow.git")
-          }
-          branch("master")
+            remote {
+                name('origin')
+                url(repoURL)
+            }
+          branch(branchName)
+          // Make sure that repository is clean and we have
+          // no leftovers from the previous builds or unsuccessful checkouts
+          wipeOutWorkspace()
         }
     }
+}
+
+/*
+ * Verify that we can build war, run unit tests and measure code coverage
+ */
+job("${GITHUB_USER}.roadshow.generated.build") {
+    // Set default parameters
+    addDefaultParameters(delegate)
+    // Add Git SCM
+    addGitSCM(delegate, "git@github.com:${GITHUB_USER}/roadshow.git")
+    // Set trigger to poll SCM every minute
     triggers {
         scm('* * * * *')
     }
+    // Actual build steps
     steps {
-        gradle('clean staticanalysis')
+        // Build war file, run tests and measure coverage
+        shell('./gradlew clean war jenkinstest jacoco')
+        // Just for fun
+        shell("echo 'Hello, world!!'")
     }
-  	publishers {
-      checkstyle('build/reports/checkstyle/*.xml')	
-      pmd('build/reports/pmd/*.xml')
-      tasks('**/*', '', 'FIXME', 'TODO', 'LOW', true)
-  	}
+    // Post build steps
+    publishers {
+        // Collect code coverage report
+        jacocoCodeCoverage()
+        // Collect unit test results
+        archiveJunit('build/test-results/*.xml')
+        // Collect compilation warnings
+        warnings(['Java Compiler (javac)'])
+        // Trigger downstream job
+        downstream("${GITHUB_USER}.roadshow.generated.staticanalysis",
+                   'SUCCESS')
+    }
+}
+
+/*
+ * Run static analysis and post results
+ */
+job("${GITHUB_USER}.roadshow.generated.staticanalysis") {
+    // Set default parameters
+    addDefaultParameters(delegate)
+    // Add Git SCM
+    addGitSCM(delegate, "git@github.com:${GITHUB_USER}/roadshow.git")
+    // Actual build steps
+    steps {
+        // Run static code analysis
+        shell('./gradlew staticanalysis')
+    }
+    // Post-build steps
+    publishers {
+        // Collect check style report
+        checkstyle('build/reports/checkstyle/*.xml')
+        // Collect PMD report
+        pmd('build/reports/pmd/*.xml')
+        // Collect tasks statistics
+        tasks('**/*', '', 'FIXME', 'TODO', 'LOW', true)
+    }
 }
