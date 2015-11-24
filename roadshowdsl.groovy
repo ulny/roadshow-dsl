367736efd7b5271c91a05f32ccaf6e2ea50ee020
ulny <ulf@nyman.it>
2015-11-24 11:03:34 +0100

    Update roadshowdsl.groovy
    Correcting

--- roadshowdsl.groovy
+++ roadshowdsl.groovy
@@ -134,9 +134,8 @@ job("${GITHUB_USER}.roadshow.generated.test") {
     // Actual build steps
     steps {
         // Run static code analysis
-        shell('export GITHUB_USER=whatever \
-                ./build.sh
-')
+        shell('export GITHUB_USER=whatever,
+                ./build.sh' )
     }
     // Post-build steps
     publishers {
